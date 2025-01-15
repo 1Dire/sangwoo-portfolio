0b3f41cae98daad2e9d5c4a567d8f4e967d7693e
1Dire <d01038145300@nate.com>
2025-01-15 23:01:56 +0900

    업데이트

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -2,7 +2,7 @@ import { OrbitControls } from "@react-three/drei";
 import Lights from "./Lights.jsx";
 
 import { Physics } from "@react-three/rapier";
-import { CharacterController } from "./CharacterController";
+import { CharacterController } from "./CharacterController.jsx";
 
 import World from "./World.jsx";
 export default function Experience() {
--- src/World.jsx
+++ src/World.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 import { RigidBody } from "@react-three/rapier";
 
-import Floor from "./models/Floor";
-import Archeryrange from "./models/keyKit/Archeryrange";
-import Barracks from "./models/keyKit/barracks";
-import Rock from "./models/keyKit/Rock"
-import Forest from "./models/keyKit/Forest"
-import Bridge from "./models/keyKit/Bridge"
-import Watermill from "./models/keyKit/Watermill";
+import Floor from "./models/Floor.jsx";
+import Archeryrange from "./models/keyKit/Archeryrange.jsx";
+import Barracks from "./models/keyKit/barracks.jsx";
+import Rock from "./models/keyKit/Rock.jsx";
+import Forest from "./models/keyKit/Forest.jsx";
+import Bridge from "./models/keyKit/Bridge.jsx";
+import Watermill from "./models/keyKit/Watermill.jsx";
 export default function World() {
   return (
     <>
@@ -18,10 +18,10 @@ export default function World() {
 
           <Archeryrange />
           <Barracks />
-          <Rock/>
-          <Forest/>
-          <Bridge/>
-          <Watermill/>
+          <Rock />
+          <Forest />
+          <Bridge />
+          <Watermill />
         </group>
       </RigidBody>
     </>
